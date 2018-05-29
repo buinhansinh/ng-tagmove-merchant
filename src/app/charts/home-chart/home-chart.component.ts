bb6b2dd07acdb05dc15a71aee948c5914d017687
K Gilmore <kgilmore.me@gmail.com>
2018-05-29 21:52:07 +0100

    fixed chart

--- src/app/charts/home-chart/home-chart.component.ts
+++ src/app/charts/home-chart/home-chart.component.ts
@@ -8,20 +8,18 @@ import homeChartData from '../../../assets/json/home-chart-data';
   styleUrls: ['./home-chart.component.scss']
 })
 export class HomeChartComponent implements OnInit {
-  lineChartData: Array<any> = homeChartData;
-  lineChartOptions: any = {
-    responsive: false,
-    maintainAspectRatio: false
-  }
+  // lineChart
+  public lineChartData:Array<any> = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
+    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+  ];
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions:any = {
+    responsive: true
+  };
+
   lineChartColors:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
     { // dark grey
       backgroundColor: 'rgba(77,83,96,0.2)',
       borderColor: 'rgba(77,83,96,1)',
@@ -29,16 +27,9 @@ export class HomeChartComponent implements OnInit {
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
   ]
+
   lineChartLegend:boolean = false;
   lineChartType:string = 'line';
 
