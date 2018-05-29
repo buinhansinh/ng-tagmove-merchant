bdbcc05c9a3007af6394a66798c1dc45b8bd7d4e
K Gilmore <kgilmore.me@gmail.com>
2018-05-29 22:12:11 +0100

    chart fix no curves

--- src/app/charts/home-chart/home-chart.component.ts
+++ src/app/charts/home-chart/home-chart.component.ts
@@ -8,16 +8,21 @@ import homeChartData from '../../../assets/json/home-chart-data';
   styleUrls: ['./home-chart.component.scss']
 })
 export class HomeChartComponent implements OnInit {
-  // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+  lineChartData: Array<any> = [
+    {
+      label: 'Offer',
+      lineTension: 0, 
+      data: [100, 80, 50, 49, 40, 39, 20]
+    }
   ];
+
   public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
-    responsive: true
-  };
+
+
+  lineChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: true,
+  }
 
   lineChartColors:Array<any> = [
     { // dark grey
