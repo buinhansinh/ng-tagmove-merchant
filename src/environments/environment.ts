de788662e154f7ceb048afe10b8455e33bf89fbf
K Gilmore <kgilmore.me@gmail.com>
2018-07-04 20:38:15 +0100

    revert change to fix heroku error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5000/api/v1/'
+    production: false
 };
