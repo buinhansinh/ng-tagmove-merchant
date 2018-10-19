01e85cb98a53a904f4d81173b6cd41111a0e8ed1
K Gilmore <kgilmore.me@gmail.com>
2018-10-19 17:42:22 +0100

    change data and logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 // *******************************************************************************
 // Layouts
 
-import { Layout1Component } from './layout/layout-1/layout-1.component';
+import { Layout2Component } from './layout/layout-2/layout-2.component';
 
 // *******************************************************************************
 // Pages
@@ -29,59 +29,59 @@ import { FaqComponent } from './faq/faq.component';
 
 const routes: Routes = [
 
-  { path: '', component: Layout1Component, pathMatch: 'full', children: [
+  { path: '', component: Layout2Component, pathMatch: 'full', children: [
     { path: '', component: HomeComponent },
   ]},
 
-  { path: 'orders', component: Layout1Component, children: [
+  { path: 'orders', component: Layout2Component, children: [
     { path: '', component: OrdersComponent },
   ]},
 
-  { path: 'order/:id', component: Layout1Component, children: [
+  { path: 'order/:id', component: Layout2Component, children: [
     { path: '', component: OrderItemComponent },
   ]},
 
-  { path: 'payouts', component: Layout1Component, children: [
+  { path: 'payouts', component: Layout2Component, children: [
     { path: '', component: PayoutsComponent },
   ]},
 
-  { path: 'payout-receipt', component: Layout1Component, children: [
+  { path: 'payout-receipt', component: Layout2Component, children: [
     { path: '', component: PayoutReceiptComponent },
   ]},
 
-  { path: 'products', component: Layout1Component, children: [
+  { path: 'products', component: Layout2Component, children: [
     { path: '', component: ProductsComponent },
   ]},
 
-  { path: 'product/:id', component: Layout1Component, children: [
+  { path: 'product/:id', component: Layout2Component, children: [
     { path: '', component: ProductItemComponent },
   ]},
 
-  { path: 'about', component: Layout1Component, children: [
+  { path: 'about', component: Layout2Component, children: [
     { path: '', component:  AboutComponent },
   ]},
 
-  { path: 'faq', component: Layout1Component, children: [
+  { path: 'faq', component: Layout2Component, children: [
     { path: '', component: FaqComponent },
   ]},
 
-  { path: 'contact', component: Layout1Component, children: [
+  { path: 'contact', component: Layout2Component, children: [
     { path: '', component: ContactComponent },
   ]},
 
-  { path: 'help', component: Layout1Component, children: [
+  { path: 'help', component: Layout2Component, children: [
     { path: '', component: HelpComponent },
   ]},
 
-  { path: 'terms', component: Layout1Component, children: [
+  { path: 'terms', component: Layout2Component, children: [
     { path: '', component: TermsComponent },
   ]},
 
-  { path: 'account-settings', component: Layout1Component, children: [
+  { path: 'account-settings', component: Layout2Component, children: [
     { path: '', component: AccountSettingsComponent },
   ]},
 
-  { path: 'add-product', component: Layout1Component, children: [
+  { path: 'add-product', component: Layout2Component, children: [
     { path: '', component: AddProductComponent },
   ]}
 
